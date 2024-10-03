96f0cd5a1d7f44a5b6f86b3cc498691222089a31
Nadia <wzmjohn2018@163.com>
2024-10-03 16:11:25 +0800

    collatz commit

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,17 +1,21 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author nadia
+ * 	1.	选择一个正整数n。
+ * 	2.	如果n是偶数，则将n除以2。
+ * 	3.	如果n是奇数，则将n乘以3并加1。
+ * 	4.	重复步骤2和3，直到n变为1。
  */
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+//        first, judge odd or even
+        if (n % 2 == 1){
+            return n * 3 + 1;
+        }else{
+            return n / 2;
         }
+
     }
 
     public static void main(String[] args) {
